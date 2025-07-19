1138151bb93f0fed8dade7a1865a504c686b43c8
ayubhonchik <ayubhonahmadjanov10@gmail.com>
2025-07-19 22:15:50 +0500

    tigirlandi

--- src/app/(laylout)/Carts/[id]/page.tsx
+++ src/app/(laylout)/Carts/[id]/page.tsx
@@ -1,24 +1,24 @@
+import React from "react";
+async function CartDetailPage({ params }: {params: {id: any}}) {
+    const id = await params.id;
+    const res = await fetch(`https://dummyjson.com/carts/${params.id}`);
+    const cart = await res.json();
 
-const CartDetailPage = async ({ params }: {params: { id: string}}) => {
-    const id = await params.id
-  const res = await fetch(`https://dummyjson.com/carts/${params.id}`);
-  const cart = await res.json();
-
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold mb-4">cart #{cart.id}</h1>
-      <ul className="grid grid-cols-2 gap-4">
-        {cart.products.map((product: any) => (
-          <li key={product.id} className="border p-3 rounded shadow">
-            <img src={product.thumbnail} alt={product.title} className="w-full h-32 object-cover mb-2 rounded" />
-            <p className="font-semibold">{product.title}</p>
-            <p>Quantity: {product.quantity}</p>
-            <p>Price: ${product.price}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    return (
+        <div className="p-6">
+            <h1 className="text-xl font-bold mb-4">cart #{cart.id}</h1>
+            <ul className="grid grid-cols-2 gap-4">
+                {cart.products.map((product: any) => (
+                    <li key={product.id} className="border p-3 rounded shadow">
+                        <img src={product.thumbnail} alt={product.title} className="w-full h-32 object-cover mb-2 rounded" />
+                        <p className="font-semibold">{product.title}</p>
+                        <p>Quantity: {product.quantity}</p>
+                        <p>Price: ${product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default CartDetailPage;
