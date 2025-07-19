ead9965dd89dbba22c2905ff3e3757c70ab04a36
ayubhonchik <ayubhonahmadjanov10@gmail.com>
2025-07-19 21:58:05 +0500

    tigirlandi

--- src/app/(laylout)/Carts/[id]/page.tsx
+++ src/app/(laylout)/Carts/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { notFound } from "next/navigation";
 
 interface PageProps {
   params: {
@@ -13,7 +12,7 @@ const CartDetailPage = async ({ params }: PageProps) => {
 
   return (
     <div className="p-6">
-      <h1 className="text-xl font-bold mb-4">🛒 Cart #{cart.id}</h1>
+      <h1 className="text-xl font-bold mb-4">cart #{cart.id}</h1>
       <ul className="grid grid-cols-2 gap-4">
         {cart.products.map((product: any) => (
           <li key={product.id} className="border p-3 rounded shadow">
