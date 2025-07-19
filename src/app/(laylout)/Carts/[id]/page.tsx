969041fc41d5af6fd327ad4a7cd1f6cd78d88029
ayubhonchik <ayubhonahmadjanov10@gmail.com>
2025-07-19 22:19:52 +0500

    changed

--- src/app/(laylout)/Carts/[id]/page.tsx
+++ src/app/(laylout)/Carts/[id]/page.tsx
@@ -1,24 +1,45 @@
 import React from "react";
-async function CartDetailPage({ params }: {params: {id: any}}) {
-    const id = await params.id;
-    const res = await fetch(`https://dummyjson.com/carts/${params.id}`);
-    const cart = await res.json();
 
-    return (
-        <div className="p-6">
-            <h1 className="text-xl font-bold mb-4">cart #{cart.id}</h1>
-            <ul className="grid grid-cols-2 gap-4">
-                {cart.products.map((product: any) => (
-                    <li key={product.id} className="border p-3 rounded shadow">
-                        <img src={product.thumbnail} alt={product.title} className="w-full h-32 object-cover mb-2 rounded" />
-                        <p className="font-semibold">{product.title}</p>
-                        <p>Quantity: {product.quantity}</p>
-                        <p>Price: ${product.price}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+async function CartDetailPage({ params }: Params) {
+  const id = params.id;
+  const res = await fetch(`https://dummyjson.com/carts/${id}`);
+  const cart = await res.json();
+
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Cart #{cart.id}</h1>
+      <ul className="grid grid-cols-2 gap-4">
+        {cart.products.map((product: any) => (
+          <li key={product.id} className="border p-3 rounded shadow">
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              className="w-full h-32 object-cover mb-2 rounded"
+            />
+            <p className="font-semibold">{product.title}</p>
+            <p>Quantity: {product.quantity}</p>
+            <p>Price: ${product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default CartDetailPage;
+
+
+export async function generateStaticParams() {
+  const res = await fetch("https://dummyjson.com/carts");
+  const data = await res.json();
+
+  return data.carts.map((cart: any) => ({
+    id: cart.id.toString(),
+  }));
+}
